8710b0d51c210f66c62e69e02f854f50e6866d2a
az17092145gary <az17092145@gmail.com>
2023-11-09 17:32:20 +0800

    1732

---
+++ BackCRM/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using BackCRM.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BackCRM.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private LoginFactory _loginFactory;
+        public LoginController(IConfiguration configuratio)
+        {
+            _loginFactory = new LoginFactory(configuratio);
+        }
+        [HttpPost]
+        public dynamic LoginCheckMember(Dictionary<string, string> account)
+        {
+            var result = _loginFactory.LoginCheckMember(account);
+            return result != null ? Ok() : NotFound();
+        }
+    }
+}
--- BackCRM/Model/LoginFactory.cs
+++ BackCRM/Model/LoginFactory.cs
@@ -1,14 +1,21 @@
-﻿using System.Data.SqlClient;
+﻿using Dapper;
+using System.Data.SqlClient;
 
 namespace BackCRM.Model
 {
     public class LoginFactory
     {
         private SqlConnection _sqlConnection;
-        public LoginFactory(IConfiguration configuration) 
+        public LoginFactory(IConfiguration configuration)
         {
             _sqlConnection = new SqlConnection(configuration.GetConnectionString("JINDI"));
         }
-        
+        public dynamic LoginCheckMember(Dictionary<string, string> account)
+        {
+            var id = account["id"];
+            var pwd = account["pwd"];
+            var result = _sqlConnection.Query($"select * from EMPL where EMPID = @id and EMPPWD = @pwd", new { id, pwd });
+            return result;
+        }
     }
 }
